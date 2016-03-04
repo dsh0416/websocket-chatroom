785cf530897d15790a8ee3a79c3d4bf745a590bc
Delton Ding <dsh0416@gmail.com>
2016-03-04 19:27:22 +0800

    Add front end

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    var vueApp = new Vue({
+        el: '#app',
+        data: {
+            has_login: false,
+            username: '用户名',
+            password: '密码',
+            message: [],
+            messageToSend: '请输入文字',
+            login: function(){
+                $.ajax({
+                    url: "/login",
+                    method: 'post',
+                    data: JSON.stringify({username: this.username, password: this.password}),
+                    success: function(data){
+                        data = JSON.parse(data);
+                        if (data['result'] != '1'){
+                            alert('密码错误');
+                        } else{
+                            vueApp.has_login = true;
+                        }
+                    }
+                })
+            },
+            send: function(){
+                ws.send('[' + new Date().toLocaleString() + '] ' + this.username + ': ' + this.messageToSend);
+            }
+        }
+    });
+
+    var ws = new WebSocket('ws://' + window.location.host + '/room/1');
+    ws.onopen    = function()  {};
+    ws.onclose   = function()  {};
+    ws.onmessage = function(m) {
+        vueApp.message.push(m.data);
+    };
+
+});
